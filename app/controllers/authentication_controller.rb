d6edf255878e6736f4fa60018f6a1d21724bfaaf
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-02-10 19:08:37 -0300

    New authentication controller

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,13 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      token = JwtService.encode({user_id: user.id})
+      render json: {token: token}, status: :ok
+    else
+      render json: {error: 'Unauthorized'}, status: :unauthorized
+    end
+  end
+end
